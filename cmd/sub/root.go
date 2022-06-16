c0d82958da1ce8f5d6af08e19ce277a54b0fd641
chenjiayao <chenjiayaooo@gmail.com>
2022-06-16 23:49:57 +0800

    :sparkles: (cmd/sub/root.go) add command args

--- cmd/sub/root.go
+++ cmd/sub/root.go
@@ -10,6 +10,9 @@ var cfgFile string
 var output string
 var format string
 var count int64
+var url string
+var tablename string
+var sleep int64
 
 var rootCmd = &cobra.Command{
 	Use: "godeng",
@@ -23,6 +26,9 @@ func init() {
 	rootCmd.PersistentFlags().StringVarP(&output, "output", "o", "stdout", "output")
 	rootCmd.PersistentFlags().StringVarP(&format, "format", "f", "json", "format")
 	rootCmd.PersistentFlags().Int64VarP(&count, "count", "n", 100, "count")
+	rootCmd.PersistentFlags().StringVarP(&url, "url", "u", "", "url")
+	rootCmd.PersistentFlags().StringVarP(&tablename, "tablename", "t", "", "tablename")
+	rootCmd.PersistentFlags().Int64VarP(&sleep, "sleep", "s", 0, "sleep")
 }
 
 func Execute() {
