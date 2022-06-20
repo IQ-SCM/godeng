4f9f768556d4e1d00f2e25f614d25d5802947d70
chenjiayao <chenjiayaooo@gmail.com>
2022-06-20 22:03:25 +0800

    :sparkles: (output/http_output.go) http output

--- output/http_output.go
+++ output/http_output.go
@@ -1,6 +1,11 @@
 package output
 
-import "github.com/chenjiayao/godeng/inter"
+import (
+	"bytes"
+	"net/http"
+
+	"github.com/chenjiayao/godeng/inter"
+)
 
 type HttpOutput struct {
 	url string
@@ -9,5 +14,13 @@ type HttpOutput struct {
 var _ inter.Output = &HttpOutput{}
 
 func (o *HttpOutput) Output(content []byte) {
+	var buf bytes.Buffer
+	buf.Read(content)
+	http.Post(o.url, "application/json", &buf)
+}
 
+func MakeHTTPOutput(url string) *HttpOutput {
+	return &HttpOutput{
+		url: url,
+	}
 }
